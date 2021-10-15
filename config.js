1859d41e057249ceffdac06b4766922ca7a00630
Icarus2903 <92596612+Icarus2903@users.noreply.github.com>
2021-10-15 23:50:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em có thể gặp được mụt người nhạt nhẽo đáng iu như c',
     btnReply: 'whyy',
     reply: 'tại vì e cute',
-    mess: 'cũng là vì e cute nma nhiều hơn 1 chút',
+    mess: 'hihi e biết mà :3',
     messDesc: 'khi nào hếch dịch cho xin cái địa chỉ nhó k bị lạc đường đấy :<',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
